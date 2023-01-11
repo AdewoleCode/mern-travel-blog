64c5fafce945a9d918734be2f429cb1016964053
adewole <adeadewole1712@gmail.com>
2023-01-11 22:04:00 +0100

    installed cors dependency to give access to frontend

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose")
+const cors = require('cors')
 
 const userRouter = require('./routes/userRoutes')
 const postRouter = require('./routes/postRoutes')
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 
 app.use("/user", userRouter)
 app.use("/post", postRouter)
