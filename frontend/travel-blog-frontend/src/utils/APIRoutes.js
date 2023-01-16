edcbdd4af8bbbaf374b551879eab41452ab99bb3
adewole <adeadewole1712@gmail.com>
2023-01-17 00:10:08 +0100

    updatinf api routes

--- frontend/travel-blog-frontend/src/utils/APIRoutes.js
+++ frontend/travel-blog-frontend/src/utils/APIRoutes.js
@@ -1,5 +1,13 @@
-export const host = "http://localhost:3030";
-export const loginRoute = `${host}/user/login`;
-export const registerRoute = `${host}/user/signup`;
-export const usersRoute = `${host}/user`;
-export const PostsRoute = `${host}/post`
+// export const host = "http://localhost:3030";
+// export const loginRoute = `${host}/user/login`;
+// export const registerRoute = `${host}/user/signup`;
+// export const usersRoute = `${host}/user`;
+// export const PostsRoute = `${host}/post`
+export const PostsRoute = `https://travel-diary-mern-backend.onrender.co/post`
+export const usersRoute = `https://travel-diary-mern-backend.onrender.com/user`;
+export const registerRoute = `https://travel-diary-mern-backend.onrender.com/user/signup`;
+export const loginRoute = `https://travel-diary-mern-backend.onrender.com/user/login`;
+
+
+
+
