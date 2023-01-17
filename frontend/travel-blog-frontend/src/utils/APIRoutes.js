4ec009e5d37ad67516ae764fb4b5ccf431809f87
adewole <adeadewole1712@gmail.com>
2023-01-17 01:16:42 +0100

    fixing routes

--- frontend/travel-blog-frontend/src/utils/APIRoutes.js
+++ frontend/travel-blog-frontend/src/utils/APIRoutes.js
@@ -1,12 +1,12 @@
-// export const host = "http://localhost:3030";
-// export const loginRoute = `${host}/user/login`;
-// export const registerRoute = `${host}/user/signup`;
-// export const usersRoute = `${host}/user`;
-// export const PostsRoute = `${host}/post`
-export const PostsRoute = `https://travel-diary-mern-backend.onrender.com/post`
-export const usersRoute = `https://travel-diary-mern-backend.onrender.com/user`;
-export const registerRoute = `https://travel-diary-mern-backend.onrender.com/user/signup`;
-export const loginRoute = `https://travel-diary-mern-backend.onrender.com/user/login`;
+export const host = "http://localhost:3030";
+export const loginRoute = `${host}/user/login`;
+export const registerRoute = `${host}/user/signup`;
+export const usersRoute = `${host}/user`;
+export const PostsRoute = `${host}/post`
+// export const PostsRoute = `https://travel-diary-mern-backend.onrender.com/post`
+// export const usersRoute = `https://travel-diary-mern-backend.onrender.com/user`;
+// export const registerRoute = `https://travel-diary-mern-backend.onrender.com/user/signup`;
+// export const loginRoute = `https://travel-diary-mern-backend.onrender.com/user/login`;
 
 
 
