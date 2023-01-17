2607ab002c10054fc469bf6b73c32e8a605a321e
adewole <adeadewole1712@gmail.com>
2023-01-17 01:12:27 +0100

    url error

--- frontend/travel-blog-frontend/src/utils/APIRoutes.js
+++ frontend/travel-blog-frontend/src/utils/APIRoutes.js
@@ -3,7 +3,7 @@
 // export const registerRoute = `${host}/user/signup`;
 // export const usersRoute = `${host}/user`;
 // export const PostsRoute = `${host}/post`
-export const PostsRoute = `https://travel-diary-mern-backend.onrender.co/post`
+export const PostsRoute = `https://travel-diary-mern-backend.onrender.com/post`
 export const usersRoute = `https://travel-diary-mern-backend.onrender.com/user`;
 export const registerRoute = `https://travel-diary-mern-backend.onrender.com/user/signup`;
 export const loginRoute = `https://travel-diary-mern-backend.onrender.com/user/login`;
