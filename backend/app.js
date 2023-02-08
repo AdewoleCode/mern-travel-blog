607b788aa21d2b33af85aa46853de43aa8fb18b2
adewole <adeadewole1712@gmail.com>
2023-02-08 15:12:48 +0100

    cors configuration

--- backend/app.js
+++ backend/app.js
@@ -7,33 +7,13 @@ const cors = require('cors')
 const userRouter = require('./routes/userRoutes')
 const postRouter = require('./routes/postRoutes')
 
-const corsOptions ={
-    origin:'http://localhost:3030', 
-    // origin:'https://travel-diary-backend.onrender.com/', 
-    credentials:true,  //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
 
-
-// app.use(cors())
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/user", userRouter)
 app.use("/post", postRouter)
-
-userRouter.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-   });
-
-
-
-
 
 
 const port = process.env.PORT || 3030;
